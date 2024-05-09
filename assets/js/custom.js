bc8b8b62b92396397e29325ea00f50f4717f1deb
ArunkumarKP <aroon.teni@gmail.com>
2024-05-09 07:35:07 +0530

    Aruna Cycle Mart specific changed added
    - products.csv will have all product information
    - jQuery <script> block will iterate on products.csv and add products dynamically
    - Accessory section added and related CSS and JS file changes are implemented.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,6 +58,25 @@
 		 }
 	 })
 
+	 $('.owl-accessory-item').owlCarousel({
+		items:5,
+		loop:true,
+		dots: true,
+		nav: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			  }
+		 }
+	 })
+
 	$(window).scroll(function() {
 	  var scroll = $(window).scrollTop();
 	  var box = $('#top').height();
